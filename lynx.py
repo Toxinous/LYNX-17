5fadb307c6cb067a6a193a262afc9ae60dadb6d3
Toxinous <toxinoustoxic@gmail.com>
2018-06-09 12:12:27 -0700

    Update lynx.py

--- lynx.py
+++ lynx.py
@@ -11,7 +11,7 @@ async def on_member_join(member):
 
     embed.set_thumbnail(url="https://orig00.deviantart.net/5bf9/f/2018/159/9/1/fidelkirbypastel_by_lahlly-dcdv76r.gif")
 
-    embed.add_field(name="Welcome to Fidel's server!", value="Thanks for joining, {}! Remember to check out #rules before chatting.".format(member.mention))
+    embed.add_field(name="Welcome to Fidel's server!", value="Thanks for joining, {}! Remember to check out <#454831454086627328> before chatting.".format(member.mention))
     embed.add_field(name="Feel free to invite your friends using the link below!", value="━ https://discord.gg/BRkYkAT ━")
 
     await client.send_message(client.get_channel("454832512582287360"), embed=embed)
